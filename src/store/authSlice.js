503bc495827bbf49af8d53563ca35caaf2f9f281
Tanisha <tanishajain2322@gmail.com>
2024-09-16 17:01:55 +0530

    first commit

---
+++ src/store/authSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+// All this is done to track authentication whether the user is authenticated or not. Thsi i will as from store everytime 
+
+
+const initialState = {
+    status : false,
+    userData: null
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+     }
+})
+
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducer;
